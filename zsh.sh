e8b0ea31b772cd65c7d4e1d6c53165c93f16a61f
Stefan Ilie <ilie.stefan92@yahoo.com>
2019-12-30 14:52:03 +0200

    add nvm

--- zsh.sh
+++ zsh.sh
@@ -2,5 +2,13 @@
 echo "\n Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+echo "- Preparing nvm"
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+echo "\n Installing node 8 via nvm"
+nvm install 8
+
 echo "\n Copying zshrc."
 cp .zshrc ~
