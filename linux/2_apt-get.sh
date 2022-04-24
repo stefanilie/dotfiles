736076de4179ae2662a274c39a064abbea280153
Stefan Ilie <ilie.stefan92@yahoo.com>
2022-04-24 22:41:53 +0100

    [linux] reconfigure dotfiles

--- linux/2_apt-get.sh
+++ linux/2_apt-get.sh
@@ -9,6 +9,12 @@ curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
 
+echo "\n- Adding albert apt-repo"
+curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
+echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_21.10/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_21.10/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
+sudo apt-get update
+
 echo ¨\n- Installing vscode¨
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
@@ -26,7 +32,11 @@ echo ¨\n- Installing games
 sudo apt-get install lutris steam
 
 echo "\n- Installing utils"
-sudo apt-get install 1password audacity gimp spotify-client telegram tilix vlc 
+sudo apt-get install 1password albert audacity deluge discord ffmpeg gimp mixxx spotify-client telegram tilix transmission vlc 
+
+
+echo "\n- Installing nordvpn"
+sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
 
 echo "\n- Installing fun stuff"
 sudo apt-get install cowsay figlet neofetch lolcat toilet
--- linux/3_dev.sh
+++ linux/3_dev.sh
@@ -1,11 +1,17 @@
+echo "\n- Installing nvm..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
 echo "- Preparing nvm."
 export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 echo "\n- Installing & defaulting to node 10 via nvm."
-nvm install 10 && nvm use default 10
+nvm install 14 && nvm use default 14
 
 echo "\n- Installing global packages."
 npm install -g expo-cli
 npm install -g jest
+
+echo "\n- Installing AndroidStudio deps."
+sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
