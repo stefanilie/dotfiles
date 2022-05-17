a442c63eec8e00023e83ac66199b1c3f8e7dd53d
Stefan Ilie <ilie.stefan92@yahoo.com>
2022-05-17 10:26:54 +0100

    [vscode-env] update code.sh

--- code.sh
+++ code.sh
@@ -3,38 +3,47 @@
 echo " Installing code packages."
 
 code --install-extension 2gua.rainbow-brackets
+code --install-extension artdiniz.quitcontrol-vscode
 code --install-extension bibhasdn.unique-lines
 code --install-extension christian-kohler.npm-intellisense
 code --install-extension christian-kohler.path-intellisense
 code --install-extension cssho.vscode-svgviewer
 code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
 code --install-extension eg2.vscode-npm-script
 code --install-extension esbenp.prettier-vscode
 code --install-extension flowtype.flow-for-vscode
 code --install-extension formulahendry.auto-close-tag
 code --install-extension formulahendry.auto-complete-tag
 code --install-extension formulahendry.auto-rename-tag
+code --install-extension GraphQL.vscode-graphql
 code --install-extension jasonnutter.search-node-modules
 code --install-extension johnpapa.vscode-peacock
 code --install-extension jtr.vscode-position
 code --install-extension karigari.chat
 code --install-extension mgmcdermott.vscode-language-babel
 code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
 code --install-extension ms-vscode.atom-keybindings
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension ms-vsliveshare.vsliveshare-audio
 code --install-extension ms-vsliveshare.vsliveshare-pack
 code --install-extension msjsdiag.vscode-react-native
 code --install-extension naumovs.color-highlight
+code --install-extension redhat.java
+code --install-extension rvest.vs-code-prettier-eslint
 code --install-extension Shan.code-settings-sync
 code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension waderyan.gitblame
 code --install-extension wayou.vscode-todo-highlight
+code --install-extension wix.vscode-import-cost
 code --install-extension wmaurer.change-case
 code --install-extension yzane.markdown-pdf
 code --install-extension yzhang.markdown-all-in-one
-code --install-extension wix.vscode-import-cost
-code --install-extension artdiniz.quitcontrol-vscode
 
 echo " Done installing code packages.\n"
