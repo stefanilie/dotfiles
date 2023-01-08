f3f3be7084a76eb5407c51968a8bd994efe32210
Stefan Ilie <ilie.stefan92@yahoo.com>
2023-01-08 18:22:51 +0000

    [linux] fix dev

--- linux/3_dev.sh
+++ linux/3_dev.sh
@@ -7,7 +7,7 @@ export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 echo "\n- Installing & defaulting to node 14 via nvm."
-nvm install 14 && nvm use default 14
+nvm install 16 && nvm use default 16
 
 echo "\n- Installing global packages."
 npm install -g expo-cli
@@ -16,16 +16,16 @@ npm install -g jest
 echo "\n- Installing AndroidStudio deps."
 sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
 
-echo "\n- Installing rust dev environment."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source $HOME/.cargo/env
+#echo "\n- Installing rust dev environment."
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#source $HOME/.cargo/env
 
-echo "\n- Installing flyctl"
-curl -L https://fly.io/install.sh | sh
+#echo "\n- Installing flyctl"
+#curl -L https://fly.io/install.sh | sh
 
 
-echo "\n- Installing broot."export FLYCTL_INSTALL="/home/stefanilie/.fly"
-  export PATH="$FLYCTL_INSTALL/bin:$PATH"
+#echo "\n- Installing broot."export FLYCTL_INSTALL="/home/stefanilie/.fly"
+#  export PATH="$FLYCTL_INSTALL/bin:$PATH"
 
 # cargo install broot
 
