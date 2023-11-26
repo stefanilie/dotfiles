1eeaacad55126ed3009ea6e1b3f1be4293b967aa
Stefan Ilie <ilie.stefan92@yahoo.com>
2023-11-26 20:50:35 +0000

    restructure install.sh

--- install.sh
+++ install.sh
@@ -2,21 +2,26 @@
 
 echo "\n Hey there. Never mind me, just setting up your system.\n"
 
+./zsh.sh
+
 ./defaults.sh
 
 ./homebrew.sh
-
-./zsh.sh
 
 ./react_native.sh
 
 ./code.sh
 
 cp .vimrc ~
-cp .gitconfig ~
-cp .gitignore.global ~
 
-# install touchbar kitty 
-# configure the two git setups
+# Prompt for email
+read -p "Enter your email: " email
+
+# Replace $EMAIL with the provided email in .gitconfig
+# Using '' after -i to avoid creating a backup on macOS
+sed -i '' "s/\$EMAIL/$email/" .gitconfig
+cp .gitconfig ~
+
+cp .gitignore.global ~
 
 echo " All done! Enjoy.\n"
--- zsh.sh
+++ zsh.sh
@@ -1,14 +1,5 @@
-
 echo "\n Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-echo "- Preparing nvm"
-export NVM_DIR="$HOME/.nvm"
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-
-echo "\n Installing node 16 via nvm"
-nvm install 16
 
 echo "\n Copying zshrc."
 cp .zshrc ~
