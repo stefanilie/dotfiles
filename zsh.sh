f57a9b2471cbfea529d47193b271cef642bfc4bf
Stefan Ilie <ilie.stefan92@yahoo.com>
2023-11-26 12:39:36 +0000

    Update mac dotfiles

--- zsh.sh
+++ zsh.sh
@@ -7,8 +7,8 @@ export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
-echo "\n Installing node 8 via nvm"
-nvm install 10
+echo "\n Installing node 16 via nvm"
+nvm install 16
 
 echo "\n Copying zshrc."
 cp .zshrc ~
